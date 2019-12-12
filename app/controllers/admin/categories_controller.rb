4645d8330424a26bbfd8ac00880779f35d9d22f5
togmund <togmund@gmail.com>
2019-12-12 20:01:28 +0000

    followed errors through the system, created files using templates from products unitl the page rendered

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,7 +3,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: Rails.configuration.admin_auth[:username], password: Rails.configuration.admin_auth[:password]
 
   def index
-    @categories = Categories.order(id: :desc).all
+    @categories = Category.order(id: :desc).all
   end
 
 end
