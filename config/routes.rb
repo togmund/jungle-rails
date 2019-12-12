a5f6268f40739d7a598d23fd31f49fe00d593008
togmund <togmund@gmail.com>
2019-12-12 22:27:15 +0000

    generated sessions controller and associated routes , controller methods and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
