359c2badad7ad207488580da2dee646ef90aa72a
togmund <togmund@gmail.com>
2019-12-12 00:03:55 +0000

    created count values in the controller, passed them to the view, scaffolded out buttons to navigate to view products and view categories (no such categories view exists)

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,10 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: Rails.configuration.admin_auth[:username], password: Rails.configuration.admin_auth[:password]
 
   def show
+    @product_count       = Product.all.length
+    puts @product_count
+    @category_count      = Category.all.length
+    puts @category_count
   end
-  
+
 end
