5d02f0f5e8d1778370d6104e7c57dc5043c0b97a
togmund <togmund@gmail.com>
2019-12-16 23:02:14 +0000

    scaffold out each test more thoroughly. going back to requirements now that I have a little practice under my belt.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,23 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  mockProduct = Product.new
+  
   describe 'Validations' do
     # validates all attributes, presence: true
     it "is valid with all valid attributes" do
-      expect(Product.new).to be_valid
+      # expect(Product.new(name:,price:,quantity:,category:)).to be_valid
     end
 
     # validates :name, presence: true
-    it "is valid with a name"
+    it "is valid with a name" do
+    mockProduct.name = "Frank Rose"
+    expect(mockProduct).to be_valid
+    end
 
     # validates :price, presence: true
-    it "is valid with a price"
+    it "is valid with a price" do
+    mockProduct.price = 42
+    expect(mockProduct).to be_valid
+    end
 
     # validates :quantity, presence: true
-    it "is valid with a quantity"
+    it "is valid with a quantity" do
+    mockProduct.quantity = 13
+    expect(mockProduct).to be_valid
+    end
 
     # validates :category, presence: true
-    it "is valid with a category"
+    it "is valid with a category" do
+    mockProduct.category = 1
+    expect(mockProduct).to be_valid
+    end
 
   end
 end
