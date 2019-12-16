b70441aa347dced85195edf8b2e4faaefed53e4c
togmund <togmund@gmail.com>
2019-12-16 22:41:46 +0000

    generated product model and scaffold of validations tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    # validates :name, presence: true
+    # validates :price, presence: true
+    # validates :quantity, presence: true
+    # validates :category, presence: true
+  end
+end
