0fea21a9bc77f5465fe129b0927da9e8c9f7dd4f
togmund <togmund@gmail.com>
2019-12-17 00:06:33 +0000

    functioning versions of all 5 tests. now checking with mentor against the conventions

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,36 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  mockProduct = Product.new
   
   describe 'Validations' do
     # validates all attributes, presence: true
-    it "is valid with all valid attributes" do
-      # expect(Product.new(name:,price:,quantity:,category:)).to be_valid
+    it "is valid with all attributes" do
+      @category = Category.create
+      @product = Product.create( 
+        name: "Frank Rose",
+        price: 42,
+        quantity: 13,
+        category: @category
+      )
+      expect(@product).to be_valid
+      expect(@product.errors.full_messages).to be_empty
     end
 
     # validates :name, presence: true
-    it "is valid with a name" do
-    mockProduct.name = "Frank Rose"
-    expect(mockProduct).to be_valid
+    it "is invalid without a name" do
+      @category = Category.create
+      @product = Product.create( 
+        name: nil,
+        price: 42,
+        quantity: 13,
+        category: @category
+      )
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     # validates :price, presence: true
-    it "is valid with a price" do
-    mockProduct.price = 42
-    expect(mockProduct).to be_valid
+    it "is invalid without a price" do
+      @category = Category.create
+      @product = Product.create( 
+        name: "Frank Rose",
+        price: nil,
+        quantity: 13,
+        category: @category
+      )
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     # validates :quantity, presence: true
-    it "is valid with a quantity" do
-    mockProduct.quantity = 13
-    expect(mockProduct).to be_valid
+    it "is invalid without a quantity" do
+      @category = Category.create
+      @product = Product.create( 
+        name: "Frank Rose",
+        price: 42,
+        quantity: nil,
+        category: @category
+      )
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     # validates :category, presence: true
-    it "is valid with a category" do
-    mockProduct.category = 1
-    expect(mockProduct).to be_valid
+    it "is invalid without a category" do
+      @product = Product.create( 
+        name: "Frank Rose",
+        price: 42,
+        quantity: 13,
+        category: nil
+      )
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
 
   end
