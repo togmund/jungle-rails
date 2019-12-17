812e982333bed8f239a30a98c210f5790380a524
togmund <togmund@gmail.com>
2019-12-17 00:42:38 +0000

    conventions checked with instructor, merging to master

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   
   describe 'Validations' do
+
     # validates all attributes, presence: true
     it "is valid with all attributes" do
       @category = Category.create
@@ -26,6 +27,8 @@ RSpec.describe Product, type: :model do
         category: @category
       )
       expect(@product.errors.full_messages).to include("Name can't be blank")
+      # Alternate syntax:
+      # expect(@product).to have(1).error_on(:name)
     end
 
     # validates :price, presence: true
@@ -65,11 +68,3 @@ RSpec.describe Product, type: :model do
 
   end
 end
-
-
-  # Each example (it) is run in isolation of others. Therefore each example will needs its own @category created and then @product initialized with that category
-  # Create an initial example that ensures that a product with all four fields set will indeed save successfully
-  # Have one example for each validation, and for each of these:
-    # Set all fields to a value but the validation field being tested to nil
-    # Test that the expect error is found within the .errors.full_messages array
-  # You should therefore have five examples defined given that you have the four validations above
